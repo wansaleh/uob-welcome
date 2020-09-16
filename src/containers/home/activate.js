4f04187ff8f36e1c11a0953bb1a6e957fd51dad7
Wan Saleh <wansaleh@gmail.com>
2020-09-16 08:11:01 +0800

    Update.

--- src/containers/home/activate.js
+++ src/containers/home/activate.js
@@ -1,21 +1,7 @@
 /* eslint-disable no-sparse-arrays */
 import React from 'react';
-import {
-  AspectRatio,
-  Box,
-  Button,
-  Flex,
-  Heading,
-  Image,
-  Link,
-  ListItem,
-  OrderedList,
-  SimpleGrid,
-  Text,
-  useTheme
-} from '@chakra-ui/core';
+import { Box, Button, Flex, Heading, Link, useTheme } from '@chakra-ui/core';
 
-import { rgba } from 'polished';
 import Container from '../../components/container';
 // import TiltCard from '../../components/tilt-card';
 // import cards from './cards.json';
@@ -28,20 +14,20 @@ const chevronProps = {
   }
 };
 
-const Chevron = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 20 20"
-    fill="currentColor"
-    {...chevronProps}
-  >
-    <path
-      fillRule="evenodd"
-      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
+// const Chevron = () => (
+//   <svg
+//     xmlns="http://www.w3.org/2000/svg"
+//     viewBox="0 0 20 20"
+//     fill="currentColor"
+//     {...chevronProps}
+//   >
+//     <path
+//       fillRule="evenodd"
+//       d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+//       clipRule="evenodd"
+//     />
+//   </svg>
+// );
 
 const Activate = () => {
   const theme = useTheme();
