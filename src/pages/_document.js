59cc14113fc3835b4ac131c79774db543c231a74
Wan Saleh <wansaleh@gmail.com>
2020-12-29 19:54:31 +0800

    Updates.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,10 +11,10 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link
+          {/* <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
             rel="stylesheet"
-          />
+          /> */}
           {/* <script
             src="https://kit.fontawesome.com/5b84ae3710.js"
             crossOrigin="anonymous"
